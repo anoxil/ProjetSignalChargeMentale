61ab16089848b0602af480c335a52783ce4a6503
Raphael Bres <rbres@ensc.fr>
2018-11-20 14:57:45 +0100

    Q212bruitblanc add moy=2

--- Q212bruitblanc.m
+++ Q212bruitblanc.m
@@ -5,8 +5,12 @@ M = 5001;
 
 temps = (1:M);
 var = 2;
-moy = 0;
-bruit = sqrt(var)*randn(1,M)+moy;
+moy1 = 0;
+bruit1 = sqrt(var)*randn(1,M)+moy1;
+moy2 = 2;
+bruit2 = sqrt(var)*randn(1,M)+moy2;
 
-subplot(2,1,1); plot(temps, bruit); xlim([20 775]); title('Représentation temporelle')
-subplot(2,1,2); spectrogram(bruit, 256, 128, 'yaxis'); xlim([20 775]); title('Représentation temps-fréquence')
+subplot(2,2,1); plot(temps, bruit1); xlim([20 775]); title('Représentation temporelle (moy=0)')
+subplot(2,2,3); spectrogram(bruit1, 256, 128, 'yaxis'); xlim([20 775]); title('Représentation temps-fréquence (moy=0)')
+subplot(2,2,2); plot(temps, bruit2); xlim([20 775]); title('Représentation temporelle (moy=2)')
+subplot(2,2,4); spectrogram(bruit2, 256, 128, 'yaxis'); xlim([20 775]); title('Représentation temps-fréquence (moy=2)')
--- main.m
+++ main.m
@@ -14,5 +14,7 @@ var = 2; %ou 3;
 moy = 0;
 bruit2 = sqrt(var)*randn(1,M)+moy;
 
+
+
 subplot(2,1,1); plot(temps, bruit2); xlim([20 775]); title('Représentation temporelle')
 subplot(2,1,2); spectrogram(bruit2, 256, 128, 'yaxis'); xlim([20 775]); title('Représentation temps-fréquence')
